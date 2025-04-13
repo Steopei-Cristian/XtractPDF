6bb17f49b014b7136630266903ec3c655c16c1ea
antoniunegrea <negreaantoniu12@gmail.com>
2025-04-13 16:10:03 +0300

    imporved layout for pdf + fixed hardcoded fields

--- app/Services/PdfService.php
+++ app/Services/PdfService.php
@@ -159,20 +159,22 @@ class PdfService
             </tr>
         </table>';
 
+        $html .= '<div style="height: 30px;"></div>';
+
         // Invoice Lines
         if ($invoiceLines = $invoice->getInvoiceLine()) {
             $html .= '<br><table cellpadding="4" cellspacing="0" border="1" style="width: 100%;">
                 <tr style="background-color: #f0f0f0;">
-                    <th>Linia</th>
-                    <th>Nume articol/Descriere articol</th>
-                    <th>Tara provenienta</th>
-                    <th>Pretul net al articolului</th>
-                    <th>Moneda</th>
-                    <th>Cantitate de baza</th>
-                    <th>Cantitate facturata</th>
-                    <th>UM</th>
-                    <th>Cota TVA</th>
-                    <th>Valoare neta</th>
+                    <th width="5%">Linia</th>
+                    <th width="35%">Nume articol/Descriere articol</th>
+                    <th width="10%">Tara provenienta</th>
+                    <th width="10%">Pretul net al articolului</th>
+                    <th width="5%">Moneda</th>
+                    <th width="8%">Cantitate de baza</th>
+                    <th width="8%">Cantitate facturata</th>
+                    <th width="5%">UM</th>
+                    <th width="5%">Cota TVA</th>
+                    <th width="9%">Valoare neta</th>
                 </tr>';
             
             $lineNumber = 1;
@@ -239,6 +241,7 @@ class PdfService
             $html .= '</table>';
         }
 
+        $html .= '<div style="height: 30px;"></div>';
         // Totals
         if ($legalMonetaryTotal = $invoice->getLegalMonetaryTotal()) {
             $html .= '<br><table cellpadding="2" cellspacing="0" border="1" style="width: 100%; font-size: 8pt;">
@@ -277,30 +280,89 @@ class PdfService
             $html .= '<td></td>';
 
             $html .= '</tr></table>';
+            $html .= '<div style="height: 10px;"></div>';
 
             // TOTAL PLATA row
-            $html .= '<table cellpadding="2" cellspacing="0" border="0" style="width: 100%; font-size: 8pt;">
+            $html .= '<table cellpadding="2" cellspacing="0" border="0" style="width: 30%; font-size: 8pt;">
                 <tr>
-                    <td><strong>TOTAL PLATA</strong></td>
-                    <td>' . $formattedAmount . '</td>
-                </tr>
-            </table>';
+                    <td style="width: 20%;"><strong>TOTAL PLATA</strong></td>';
+            if ($legalMonetaryTotal = $invoice->getLegalMonetaryTotal()) {
+                if ($payableAmount = $legalMonetaryTotal->getPayableAmount()) {
+                    $amount = is_object($payableAmount) ? (string)$payableAmount : $payableAmount;
+                    $formattedAmount = number_format((float)$amount, 2);
+                    $html .= '<td style="text-align: right;">' . $formattedAmount . '</td>';
+                } else {
+                    $html .= '<td style="text-align: right;">0.00</td>';
+                }
+            }
+            $html .= '</tr></table>';
 
-            // TOTAL row with 0.00 RON
-            $html .= '<table cellpadding="2" cellspacing="0" border="0" style="width: 100%; font-size: 8pt;">
+            // TOTAL row with currency
+            $html .= '<div style="height: 5px;"></div>';
+            $html .= '<table cellpadding="2" cellspacing="0" border="0" style="width: 30%; font-size: 8pt;">
                 <tr>
-                    <td><strong>TOTAL</strong></td>
-                    <td>0.00</td>
-                    <td>RON</td>
+                    <td style="width: 20%;"><strong>TOTAL</strong></td>';
+            if ($legalMonetaryTotal = $invoice->getLegalMonetaryTotal()) {
+                if ($payableAmount = $legalMonetaryTotal->getPayableAmount()) {
+                    $amount = is_object($payableAmount) ? (string)$payableAmount : $payableAmount;
+                    $formattedAmount = number_format((float)$amount, 2);
+                    $html .= '<td style="text-align: right;">' . $formattedAmount . '</td>';
+                } else {
+                    $html .= '<td style="text-align: right;">0.00</td>';
+                }
+            }
+            $html .= '<td style="padding-left: 5px;">' . $invoice->getDocumentCurrencyCode() . '</td>
                 </tr>
             </table>';
 
             // DETALIEREA TVA section
-            $html .= '<table cellpadding="2" cellspacing="0" border="0" style="width: 100%; font-size: 8pt;">
+            $html .= '<div style="height: 10px;"></div>';
+            $html .= '<table cellpadding="0" cellspacing="0" border="0" style="width: 40%; font-size: 8pt;">
                 <tr>
                     <td colspan="4"><strong>DETALIEREA TVA</strong></td>
-                </tr>
-                <tr>
+                </tr>';
+            
+            if ($taxTotals = $invoice->getTaxTotal()) {
+                foreach ($taxTotals as $taxTotal) {
+                    if ($subtotals = $taxTotal->getTaxSubtotal()) {
+                        foreach ($subtotals as $subtotal) {
+                            $categoryCode = '';
+                            $taxRate = '';
+                            $taxableAmount = '';
+                            $taxAmount = '';
+
+                            if ($taxCategory = $subtotal->getTaxCategory()) {
+                                $categoryCode = $taxCategory->getID() ?: 'deO';
+                                if ($percent = $taxCategory->getPercent()) {
+                                    $taxRate = number_format((float)$percent, 2) . '%';
+                                }
+                            }
+
+                            if ($taxableAmountObj = $subtotal->getTaxableAmount()) {
+                                $taxableAmount = number_format((float)(is_object($taxableAmountObj) ? (string)$taxableAmountObj : $taxableAmountObj), 2);
+                            }
+
+                            if ($taxAmountObj = $subtotal->getTaxAmount()) {
+                                $taxAmount = number_format((float)(is_object($taxAmountObj) ? (string)$taxAmountObj : $taxAmountObj), 2);
+                            }
+
+                            $html .= '<tr>
+                                <td>Codul categoriei</td>
+                                <td>' . $categoryCode . '</td>
+                                <td>Cota TVA</td>
+                                <td>' . $taxRate . '</td>
+                            </tr>
+                            <tr>
+                                <td>Baza de calcul</td>
+                                <td>' . $taxableAmount . '</td>
+                                <td>Valoare TVA</td>
+                                <td>' . $taxAmount . '</td>
+                            </tr>';
+                        }
+                    }
+                }
+            } else {
+                $html .= '<tr>
                     <td>Codul categoriei</td>
                     <td>deO</td>
                     <td>Cota TVA</td>
@@ -308,20 +370,62 @@ class PdfService
                 </tr>
                 <tr>
                     <td>Baza de calcul</td>
-                    <td>' . $formattedAmount . '</td>
+                    <td>' . ($formattedAmount ?? '0.00') . '</td>
                     <td>Valoare TVA</td>
                     <td>0.00</td>
-                </tr>
-                <tr>
-                    <td colspan="4">Codul VATEX-EU-O motivul/Motivul scutirii: Entitatea nu este inregistrata in scopuri de TVA</td>
-                </tr>
+                </tr>';
+            }
+            
+            $html .= '<tr>
+                <td colspan="4">Codul VATEX-EU-O motivul/Motivul scutirii: Entitatea nu este inregistrata in scopuri de TVA</td>
+            </tr>
             </table>';
         }
 
         // Payment Instructions
+        $html .= '<div style="height: 15px;"></div>';
         $html .= '<br><table cellpadding="4" cellspacing="0" border="0" style="width: 100%;">
-            <tr><td colspan="2"><strong>Instructiuni de plata</strong></td></tr>
-            <tr>
+            <tr><td colspan="2"><strong>Instructiuni de plata</strong></td></tr>';
+
+        if ($paymentMeans = $invoice->getPaymentMeans()) {
+            foreach ($paymentMeans as $payment) {
+                // Payment type code
+                if ($paymentMeansCode = $payment->getPaymentMeansCode()) {
+                    $html .= '<tr>
+                        <td style="width: 30%;">Codul tipului de instrument de plata</td>
+                        <td>' . $paymentMeansCode . '</td>
+                    </tr>';
+                }
+
+                // Payment account
+                if ($payeeFinancialAccount = $payment->getPayeeFinancialAccount()) {
+                    if ($id = $payeeFinancialAccount->getID()) {
+                        $html .= '<tr>
+                            <td>Nr. cont de plata</td>
+                            <td>' . $id . '</td>
+                        </tr>';
+                    }
+
+                    if ($name = $payeeFinancialAccount->getName()) {
+                        $html .= '<tr>
+                            <td>Numele contului de plata</td>
+                            <td>' . $name . '</td>
+                        </tr>';
+                    }
+
+                    if ($financialInstitutionBranch = $payeeFinancialAccount->getFinancialInstitutionBranch()) {
+                        if ($name = $financialInstitutionBranch->getName()) {
+                            $html .= '<tr>
+                                <td>Banca</td>
+                                <td>' . $name . '</td>
+                            </tr>';
+                        }
+                    }
+                }
+            }
+        } else {
+            // Default payment instructions if none are provided
+            $html .= '<tr>
                 <td style="width: 30%;">Codul tipului de instrument de plata</td>
                 <td>42</td>
             </tr>
@@ -332,8 +436,9 @@ class PdfService
             <tr>
                 <td>Numele contului de plata</td>
                 <td>BANCA TRANSILVANIA</td>
-            </tr>
-        </table>';
+            </tr>';
+        }
+        $html .= '</table>';
 
         // Output the HTML content
         $pdf->writeHTML($html, true, false, true, false, '');
@@ -344,4 +449,4 @@ class PdfService
 
         return $pdfPath;
     }
-} 
+}
